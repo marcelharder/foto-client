1af4378000a9a6d97e342d5886fe0b38f1bdce9f
marcelharder <m_harder88@yahoo.com>
2024-09-30 21:14:55 +0200

    startin again

---
+++ src/app/dias/full-image/full-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './full-image.component.html',
+  styleUrl: './full-image.component.css'
+})
+export class FullImageComponent {
+
+}
